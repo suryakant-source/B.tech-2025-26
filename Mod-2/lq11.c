eb7a749af011f0b6b34d3ef1bc37d45c1aa8ed4a
suryakant-source <xbsuryakant@gmail.com>
2025-10-02 11:13:05 +0530

    Create lq11.c

---
+++ Mod-2/lq11.c
@@ -0,0 +1,16 @@
+/*
+Q11) Find sum of all even numbers in a given range.
+*/
+#include <stdio.h>
+
+int main() {
+    int start, end, sum = 0;
+    printf("Enter start and end of range: ");
+    scanf("%d %d", &start, &end);
+
+    for(int i = start; i <= end; i++)
+        if(i % 2 == 0) sum += i;
+
+    printf("Sum of even numbers: %d\n", sum);
+    return 0;
+}
