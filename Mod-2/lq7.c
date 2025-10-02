c6c5027b5871831934e9c8e056329924bb56d26f
suryakant-source <xbsuryakant@gmail.com>
2025-10-02 11:02:47 +0530

    Create lq7.c

---
+++ Mod-2/lq7.c
@@ -0,0 +1,15 @@
+/*
+Q7) Find the sum of all user entered numbers until the sum exceeds 100 using do-while loop.
+*/
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0;
+    do {
+        printf("Enter a number: ");
+        scanf("%d", &num);
+        sum += num;
+    } while(sum <= 100);
+    printf("Sum exceeded 100: %d\n", sum);
+    return 0;
+}
