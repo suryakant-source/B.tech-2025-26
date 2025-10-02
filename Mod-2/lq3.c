61d8290af49cd40716530270782847c29e517895
suryakant-source <xbsuryakant@gmail.com>
2025-10-02 10:56:14 +0530

    Create lq3.c

---
+++ Mod-2/lq3.c
@@ -0,0 +1,17 @@
+/*
+Q3) Find sum of digits of an entered number using while loop.
+*/
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while(n != 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+    printf("Sum of digits: %d\n", sum);
+    return 0;
+}
