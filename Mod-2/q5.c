97a39dee5fa20741e90fe49c8a77121ce4d519e0
suryakant-source <xbsuryakant@gmail.com>
2025-10-02 08:37:18 +0530

    Create q5.c

---
+++ Mod-2/q5.c
@@ -0,0 +1,18 @@
+/*
+Q5) Check whether an entered year is a leap year or not.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int year;
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+        printf("%d is a Leap Year\n", year);
+    else
+        printf("%d is not a Leap Year\n", year);
+
+    return 0;
+}
