4c9f8c7e46cbe1d151d01a9be114fd8ff0fa3171
suryakant-source <xbsuryakant@gmail.com>
2025-10-03 07:20:58 +0530

    Create lq20.c

---
+++ Mod-2/lq20.c
@@ -0,0 +1,26 @@
+/*
+Q20) Compute reverse of a number and check whether the number is palindrome or not.
+*/
+#include <stdio.h>
+
+int main() {
+    int n, rev = 0, original, digit;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    original = n;
+
+    while(n != 0) {
+        digit = n % 10;
+        rev = rev * 10 + digit;
+        n /= 10;
+    }
+
+    printf("Reversed number: %d\n", rev);
+    if(original == rev)
+        printf("%d is a Palindrome number\n", original);
+    else
+        printf("%d is not a Palindrome number\n", original);
+
+    return 0;
+}
+u
