7f1f6fa0ca8880ef7c0d7ba19e1fbec68686e94c
suryakant-source <xbsuryakant@gmail.com>
2025-10-03 10:27:40 +0530

    Create lq38.c

---
+++ Mod-2/lq38.c
@@ -0,0 +1,16 @@
+/* 38. Print the following pattern:
+    *
+   * *
+  * * *
+ * * * *
+* * * * * */
+#include <stdio.h>
+int main() {
+    int i,j,space,n=5;
+    for(i=1;i<=n;i++) {
+        for(space=1;space<=n-i;space++) printf(" ");
+        for(j=1;j<=i;j++) printf("* ");
+        printf("\n");
+    }
+    return 0;
+}
