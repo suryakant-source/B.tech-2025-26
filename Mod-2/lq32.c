0384f5ec32f72958c06d79a1831e6bf8ae8db6f6
suryakant-source <xbsuryakant@gmail.com>
2025-10-03 07:57:13 +0530

    Create lq32.c

---
+++ Mod-2/lq32.c
@@ -0,0 +1,14 @@
+/* 32. Print the following pattern:
+1
+1 2 1
+1 2 3 2 1 */
+#include <stdio.h>
+int main() {
+    int i,j,n=3;
+    for(i=1;i<=n;i++) {
+        for(j=1;j<=i;j++) printf("%d ",j);
+        for(j=i-1;j>=1;j--) printf("%d ",j);
+        printf("\n");
+    }
+    return 0;
+}
