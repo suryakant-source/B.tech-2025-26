6f8613845ae42b29482a1a8bf6cfdf34bf2c13f8
suryakant-source <xbsuryakant@gmail.com>
2025-10-03 07:26:01 +0530

    Create lq25.c

---
+++ Mod-2/lq25.c
@@ -0,0 +1,16 @@
+/*
+Q25) Print multiplication table of a number.
+*/
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Multiplication table of %d:\n", n);
+    for(int i = 1; i <= 10; i++)
+        printf("%d x %d = %d\n", n, i, n*i);
+
+    return 0;
+}
